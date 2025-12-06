7a5ec3a0b1fbf843deebbf104893cfa0a2406dfa
Vahid Saadat <vst121@gmail.com>
2025-12-06 22:43:23 +0100

    Check if an item is in Cart

--- src/app/features/fda-enforcement/fda-enforcement.component.ts
+++ src/app/features/fda-enforcement/fda-enforcement.component.ts
@@ -11,6 +11,8 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { addToCart } from '../../core/store/cart/cart.actions';
 
+import { selectCartItems } from '../../core/store/cart/cart.selectors';
+
 @Component({
     selector: 'app-fda-enforcement',
     standalone: true,
@@ -28,6 +30,7 @@ export class FdaEnforcementComponent implements OnInit {
     reports$: Observable<FdaEnforcementResult[]> | undefined;
     loading$: Observable<boolean> | undefined;
     error$: Observable<any> | undefined;
+    cartItems$: Observable<FdaEnforcementResult[]> | undefined;
 
     constructor(private store: Store) { }
 
@@ -36,9 +39,15 @@ export class FdaEnforcementComponent implements OnInit {
         this.reports$ = this.store.select(selectReports);
         this.loading$ = this.store.select(selectLoading);
         this.error$ = this.store.select(selectError);
+        this.cartItems$ = this.store.select(selectCartItems);
     }
 
     addToCart(item: FdaEnforcementResult) {
         this.store.dispatch(addToCart({ item }));
+    }
+
+    isInCart(item: FdaEnforcementResult, cartItems: FdaEnforcementResult[] | null): boolean {
+        if (!cartItems) return false;
+        return cartItems.some(c => c.event_id === item.event_id);
     }
 }
